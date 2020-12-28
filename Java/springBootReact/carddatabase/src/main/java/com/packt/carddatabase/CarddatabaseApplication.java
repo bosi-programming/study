ef754c875334ecd7ff3dca9406b49a8747477159
fbosi <bosisoftwareengineering@protonmail.com>
2020-12-28 18:35:17 -0300

    Add data about node and Java

---
+++ Java/springBootReact/carddatabase/src/main/java/com/packt/carddatabase/CarddatabaseApplication.java
@@ -0,0 +1,17 @@
+package com.packt.carddatabase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarddatabaseApplication {
+  private static final Logger logger = LoggerFactory.getLogger(CarddatabaseApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarddatabaseApplication.class, args);
+    logger.info("Hello");
+	}
+
+}
--- node/begginingNode/l2/hello-hapi/routes/todo.js
+++ node/begginingNode/l2/hello-hapi/routes/todo.js
@@ -1,4 +1,5 @@
-const Joi = require('joi');
+const Joi = require("joi");
+const Knex = require("../db");
 
 const todoList = [
   {
@@ -25,30 +26,32 @@ const todoList = [
 
 module.exports = [
   {
-    method: 'GET',
-    path: '/todo',
+    method: "GET",
+    path: "/todo",
     handler: (request, reply) => {
       return reply(todoList);
     },
   },
   {
-    method: 'GET',
-    path: '/todo/{id}',
+    method: "GET",
+    path: "/todo/{id}",
     handler: (request, reply) => {
       const id = request.params.id - 1; // since array is 0-based index
       // should return 404 error if item
       // is not found
       if (todoList[id]) return reply(todoList[id]);
-      return reply({ message: 'Not found' }).code(404);
+      return reply({ message: "Not found" }).code(404);
     },
   },
   {
-    method: 'POST',
-    path: '/todo',
-    handler: (request, reply) => {
+    method: "POST",
+    path: "/todo",
+    handler: async (request, reply) => {
       const todo = request.payload;
+      todo.user_id = 1;
+      const [todoId] = await Knex("todo").returning("id").insert(todo);
       todoList.push(todo);
-      return reply({ message: 'created' });
+      return reply({ message: "created", todo_id: todoId });
     },
     config: {
       validate: {
@@ -59,39 +62,56 @@ module.exports = [
     },
   },
   {
-    method: 'PUT',
-    path: '/todo/{id}',
+    method: "POST",
+    path: "/todo/{id}/item",
+    handler: async (request, reply) => {
+      const todoItem = request.payload;
+      todoItem.todo_id = request.params.id;
+      const [id] = await Knex("todo_item").insert(todoItem);
+      return reply({ message: "created", id: id });
+    },
+    config: {
+      validate: {
+        payload: {
+          text: Joi.string().required(),
+        },
+      },
+    },
+  },
+  {
+    method: "PUT",
+    path: "/todo/{id}",
     handler: (request, reply) => {
       const index = request.params.id - 1;
       // replace the whole resource with the new one
       todoList[index] = request.payload;
-      return reply({ message: 'updated' });
+      return reply({ message: "updated" });
     },
   },
   {
-    method: 'PATCH',
-    path: '/todo/{id}',
+    method: "PATCH",
+    path: "/todo/{id}",
     handler: (request, reply) => {
       const index = request.params.id - 1;
       // item to be patched
       const todo = todoList[index];
-      // for each key provided, update on 
+      // for each key provided, update on
       // the resource
-      Object.keys(request.payload).forEach(key => {
+      Object.keys(request.payload).forEach((key) => {
         if (key in todo) {
           todo[key] = request.payload[key];
         }
       });
-      return reply({ message: 'patched' });
+      return reply({ message: "patched" });
     },
   },
   {
-    method: 'DELETE',
-    path: '/todo/{id}',
+    method: "DELETE",
+    path: "/todo/{id}",
     handler: (request, reply) => {
       const index = request.params.id - 1;
       delete todoList[index]; // replaces with `undefined`
-      return reply({ message: 'deleted' });
+      return reply({ message: "deleted" });
     },
   },
 ];
