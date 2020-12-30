8decd07addc577fe4619e560cb46ff9435067af6
fbosi <bosisoftwareengineering@protonmail.com>
2020-12-30 09:14:01 -0300

    Continue Java Study

--- Java/springBootReact/carddatabase/src/main/java/com/packt/carddatabase/CarddatabaseApplication.java
+++ Java/springBootReact/carddatabase/src/main/java/com/packt/carddatabase/CarddatabaseApplication.java
@@ -1,17 +1,50 @@
 package com.packt.carddatabase;
 
+import com.packt.carddatabase.domain.Car;
+import com.packt.carddatabase.domain.CarRepository;
+import com.packt.carddatabase.domain.Owner;
+import com.packt.carddatabase.domain.OwnerRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CarddatabaseApplication {
   private static final Logger logger = LoggerFactory.getLogger(CarddatabaseApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarddatabaseApplication.class, args);
+  @Autowired
+  private CarRepository repository;
+  @Autowired
+  private OwnerRepository orepository;
+
+  public static void main(String[] args) {
+    SpringApplication.run(CarddatabaseApplication.class, args);
     logger.info("Hello");
-	}
+  }
+
+  @Bean
+
+  CommandLineRunner runner() {
+    return args -> {
+      // Add owner objects and save these to db
+      Owner owner1 = new Owner("John", "Johnson");
+      Owner owner2 = new Owner("Mary", "Robinson");
+      orepository.save(owner1);
+      orepository.save(owner2);
+
+      // Add car object with link to owners and save these to db.
+      Car car = new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1);
+      repository.save(car);
+      car = new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2);
+      repository.save(car);
+      car = new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2);
+      repository.save(car);
+    };
+  }
 
 }
--- Java/springBootReact/carddatabase/src/main/java/com/packt/carddatabase/domain/Car.java
+++ Java/springBootReact/carddatabase/src/main/java/com/packt/carddatabase/domain/Car.java
@@ -1,9 +1,12 @@
 package com.packt.carddatabase.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Car {
@@ -16,7 +19,7 @@ public class Car {
   public Car() {
   }
 
-  public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+  public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
     super();
     this.brand = brand;
     this.model = model;
@@ -24,6 +27,19 @@ public class Car {
     this.registerNumber = registerNumber;
     this.year = year;
     this.price = price;
+    this.owner = owner;
+  }
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "owner")
+  private Owner owner;
+
+  public Owner getOwner() {
+    return owner;
+  }
+
+  public void setOwner(Owner owner) {
+    this.owner = owner;
   }
 
   public String getBrand() {
@@ -73,4 +89,5 @@ public class Car {
   public void setPrice(int price) {
     this.price = price;
   }
+
 }
