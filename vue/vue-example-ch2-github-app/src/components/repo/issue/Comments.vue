2c8c60038e72e07a6a0e5ff2a1c78f317537a058
bosifullstack <bosiarquitetura@gmail.com>
2022-01-17 08:51:32 -0300

    Working on the vue github app

--- vue/vue-example-ch2-github-app/src/components/repo/issue/Comments.vue
+++ vue/vue-example-ch2-github-app/src/components/repo/issue/Comments.vue
@@ -10,7 +10,68 @@
 </template>
 
 <script>
+import { octokitMixin } from "../../../mixins/octokitMixin";
+
 export default {
-  
-}
+  name: "IssueComments",
+  props: {
+    owner: {
+      type: String,
+      required: true,
+    },
+    repo: {
+      type: String,
+      required: true,
+    },
+    issueNumber: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      comments: [],
+    };
+  },
+  mixins: [octokitMixin],
+  methods: {
+    async getIssueComments(owner, repo, issueNumber) {
+      if (
+        typeof owner !== "string" ||
+        typeof repo !== "string" ||
+        typeof issueNumber !== "number"
+      ) {
+        return;
+      }
+
+      const octokit = this.createOctokitClient();
+      const { data: comments } = await octokit.issues.listComments({
+        owner,
+        repo,
+        issue_number: issueNumber,
+      });
+      this.comments = comments;
+    },
+    watch: {
+      owner: {
+        immediate: true,
+        handler(val) {
+          this.getIssueComments(val, this.repo, this.issueNumber);
+        },
+      },
+      repo: {
+        immediate: true,
+        handler(val) {
+          this.getIssueComments(this.owner, val, this.issueNumber);
+        },
+      },
+      issueNumber: {
+        immediate: true,
+        handler(val) {
+          this.getIssueComments(this.owner, this.repo, val);
+        },
+      },
+    },
+  },
+};
 </script>
