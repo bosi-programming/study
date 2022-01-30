97671d95e46955dffc01d06a0740b1bc92940bdb
bosifullstack <bosiarquitetura@gmail.com>
2022-01-30 11:32:26 -0300

    Studing vue + laravel

---
+++ vue/vue-example-ch8-chat-app/backend/app/Models/Message.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    protected $fillable = [
+        'message', 'chat_id', 'user_id',
+    ];
+
+    public function chat()
+    {
+        return $this->belongsTo('App\Models\Chat');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
